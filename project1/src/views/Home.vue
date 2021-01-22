93d25c3cccbef60e249fa6dab8f7df1911388c8f
Anastasia Nikiforova <nastasya.nikiforova@gmail.com>
2021-01-22 16:38:44 +0300

    initial project

---
+++ project1/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <h1>Initial Project</h1>
+    <p>Despite creating by command: "vue create project1 --no-git --bare",  views "About" and "Home" were generated</p>
+    <p>:)</p>
+  </div>
+</template>
